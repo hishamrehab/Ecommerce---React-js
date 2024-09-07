cfc163715662f90e8e97811380346abe394a1415
My Name <hishamrehab309@gmail.com>
2024-09-07 13:43:10 +0300

    first commit

---
+++ FrontEnd/src/App.jsx
@@ -0,0 +1,16 @@
+import SignUp from './SignUp'
+import viteLogo from '/vite.svg'
+import './App.css'
+
+function App() {
+  return (
+    <>
+      <SignUp />
+
+
+
+    </>
+  )
+}
+
+export default App
